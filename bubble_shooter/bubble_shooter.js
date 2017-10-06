b45d8505c66ce328e9015c8af3affa7e02d83b42
Nathaniel Wu <snipernathaniel@gmail.com>
2017-10-06 04:02:12 -0400

    Bubble shooter completed.

--- bubble_shooter/bubble_shooter.js
+++ bubble_shooter/bubble_shooter.js
@@ -4,27 +4,51 @@ const shooter_width = 100;
 const bubble_colors = [
     { 'r': 255, 'g': 0, 'b': 0 },
     { 'r': 0, 'g': 255, 'b': 0 },
-    { 'r': 0, 'g': 0, 'b': 255 }
+    { 'r': 0, 'g': 0, 'b': 255 },
+    { 'r': 255, 'g': 255, 'b': 0 },
+    { 'r': 0, 'g': 255, 'b': 255 },
+    { 'r': 255, 'g': 0, 'b': 255 }
 ];
+const interval_between_bubble_generation = 1;
 class BubbleShooter extends Game {
     constructor() {
         super(60);
         this.fixed_bubbles = null;
         this.moving_bubbles = null;
         this.shooter = null;
+        this.current_max_row = null;
+        this.max_allowed_row = null;
+        this.previous_loop = null;
     }
 
-    init() {
-        super.init();
+    load() {
         max_col_0 = Math.floor(canvas.width / (2 * bubble_radius));
         max_col_1 = Math.floor((canvas.width - bubble_radius) / (2 * bubble_radius));
         max_row = Math.floor(canvas.height / (sqrt_of_3 * bubble_radius));
+        this.current_max_row = 0;
+        this.max_allowed_row = Math.round(this.canvas.height * 0.8 / (2 * bubble_radius));
         this.fixed_bubbles = new Array();
         this.moving_bubbles = new Array();
         this.shooter = new Shooter(Math.round((this.canvas.width - shooter_width) / 2), 0, shooter_width);
         this.shooter.coord.y = this.canvas.height - this.shooter.get_node_by_name("base").content.h;
         for (var i = 0; i < 3; i++)
             this.generate_row_of_fixed_bubbles(i);
+        this.previous_loop = 0;
+    }
+
+    deload() {
+        super.deload();
+        for (var i = this.fixed_bubbles.length - 1; i >= 0; i--)
+            game.fixed_bubbles.splice(i, 1)[0].destroy();
+        this.fixed_bubbles = null;
+        for (var i = this.moving_bubbles.length - 1; i >= 0; i--)
+            game.moving_bubbles.splice(i, 1)[0].destroy();
+        this.moving_bubbles = null;
+        this.shooter.destroy();
+        this.shooter = null;
+        this.current_max_row = null;
+        this.max_allowed_row = null;
+        this.previous_loop = null;
     }
 
     update() {
@@ -36,23 +60,50 @@ class BubbleShooter extends Game {
                 this.moving_bubbles[i].update();
         if (this.shooter)
             this.shooter.update();
+        if (this.previous_loop > this.framerate * interval_between_bubble_generation) {
+            this.move_fixed_bubbles_down();
+            this.generate_row_of_fixed_bubbles(0);
+            this.previous_loop = 0;
+        }
     }
 
     draw() {
-        this.canvas.width = this.canvas.width;
+        super.draw();
+        //Frame
+        context.beginPath();
+        context.strokeStyle = "grey";
+        context.lineWidth = 1;
+        context.rect(0, 0, this.canvas.width, this.canvas.height);
+        context.stroke();
+        //Line
+        context.beginPath();
+        context.strokeStyle = "grey";
+        context.lineWidth = 1;
+        context.moveTo(0, this.max_allowed_row * 2 * bubble_radius);
+        context.lineTo(this.canvas.width, this.max_allowed_row * 2 * bubble_radius);
+        context.stroke();
+        //Shooter
+        if (this.shooter)
+            this.shooter.draw();
+        //Bubbles
         if (this.fixed_bubbles)
             for (var i = 0; i < this.fixed_bubbles.length; i++)
                 this.fixed_bubbles[i].draw();
         if (this.moving_bubbles)
             for (var i = 0; i < this.moving_bubbles.length; i++)
                 this.moving_bubbles[i].draw();
-        if (this.shooter)
-            this.shooter.draw();
-        context.beginPath();
-        context.strokeStyle = "grey";
-        context.lineWidth = 1;
-        context.rect(0, 0, canvas.width, canvas.height);
-        context.stroke();
+    }
+
+    loop() {
+        super.loop();
+        this.previous_loop++;
+        if (this.current_max_row > this.max_allowed_row + 1) {
+            this.stop();
+            alert("Game Over!");
+            this.load();
+            this.start_loop();
+        } else
+            this.current_max_row = 0;
     }
 
     add_more_bubble() {
@@ -60,9 +111,42 @@ class BubbleShooter extends Game {
         this.generate_row_of_fixed_bubbles(0);
     }
 
-    move_fixed_bubbles_down() {
-        upper_left_bubble_coord.x = upper_left_bubble_coord.x == bubble_radius ? 2 * bubble_radius : bubble_radius;
+    move_fixed_bubbles_down() {/*
+        var no_move = new Array();
         for (var i = 0; i < this.fixed_bubbles.length; i++) {
+            if (this.fixed_bubbles[i].row == 0)
+                continue;
+            var higher_rcs;
+            if ((upper_left_bubble_coord.x == bubble_radius && this.fixed_bubbles[i].row % 2 == 0) || (upper_left_bubble_coord.x != bubble_radius && this.fixed_bubbles[i].row % 2 != 0))
+                higher_rcs = [{ 'row': this.fixed_bubbles[i].row - 1, 'col': this.fixed_bubbles[i].col - 1 }, { 'row': this.fixed_bubbles[i].row - 1, 'col': this.fixed_bubbles[i].col }];
+            else
+                higher_rcs = [{ 'row': this.fixed_bubbles[i].row - 1, 'col': this.fixed_bubbles[i].col }, { 'row': this.fixed_bubbles[i].row - 1, 'col': this.fixed_bubbles[i].col + 1 }];
+            var move = false;
+            for (var j = 0; j < higher_rcs.length; j++) {
+                var res = game.fixed_bubble_at(higher_rcs[j]);
+                if (res != null) {
+                    move = true;
+                    for (var k = 0; k < no_move.length; k++)
+                        if (no_move[k] == res) {
+                            move = false;
+                            break;
+                        }
+                    break;
+                }
+            }
+            if (!move)
+                no_move.push(i);
+        }*/
+        upper_left_bubble_coord.x = upper_left_bubble_coord.x == bubble_radius ? 2 * bubble_radius : bubble_radius;
+        for (var i = 0; i < this.fixed_bubbles.length; i++) {/*
+            var skip = false;
+            for (var j = 0; j < no_move.length; j++)
+                if (i == no_move[j]) {
+                    skip = true;
+                    break;
+                }
+            if (skip)
+                continue;*/
             this.fixed_bubbles[i].row++;
             this.fixed_bubbles[i].coord = Bubble.row_and_col_to_coord({ 'row': this.fixed_bubbles[i].row, 'col': this.fixed_bubbles[i].col });
         }
@@ -79,12 +163,33 @@ class BubbleShooter extends Game {
             this.fixed_bubbles.push(bubble);
         }
     }
+
+    stop_keeping_fixed_bubble(bubble) {
+        for (var i = 0; i < this.fixed_bubbles.length; i++)
+            if (this.fixed_bubbles[i] === bubble)
+                return this.fixed_bubbles.splice(i, 1)[0];
+        return null;
+    }
+
+    stop_keeping_moving_bubble(bubble) {
+        for (var i = 0; i < this.moving_bubbles.length; i++)
+            if (this.moving_bubbles[i] === bubble)
+                return this.moving_bubbles.splice(i, 1)[0];
+        return null;
+    }
+
+    fixed_bubble_at(rc) {
+        for (var i = 0; i < this.fixed_bubbles.length; i++)
+            if (this.fixed_bubbles[i].row == rc.row && this.fixed_bubbles[i].col == rc.col)
+                return i;
+        return null;
+    }
 }
 
 //---------------------------------------------- Bubble Sprite
 
 const bubble_radius = 25;
-const bubble_speed = 1;
+const bubble_speed = 3;
 const min_distance = bubble_speed / bubble_radius;
 const sqrt_of_3 = Math.sqrt(3);
 var max_col_0 = Math.floor(canvas.width / (2 * bubble_radius));
@@ -166,26 +271,65 @@ class Bubble extends Sprite {
         return new Bubble(this.coord.x, this.coord.y, this.radius, 0, 0, 0, 0);
     }
 
+    remove(force) {
+        var surrounding_rcs;
+        if ((upper_left_bubble_coord.x == bubble_radius && this.row % 2 == 0) || (upper_left_bubble_coord.x != bubble_radius && this.row % 2 != 0))
+            surrounding_rcs = [{ 'row': this.row - 1, 'col': this.col - 1 }, { 'row': this.row - 1, 'col': this.col }, { 'row': this.row, 'col': this.col - 1 }, { 'row': this.row, 'col': this.col + 1 }, { 'row': this.row + 1, 'col': this.col - 1 }, { 'row': this.row + 1, 'col': this.col }];
+        else
+            surrounding_rcs = [{ 'row': this.row - 1, 'col': this.col }, { 'row': this.row - 1, 'col': this.col + 1 }, { 'row': this.row, 'col': this.col - 1 }, { 'row': this.row, 'col': this.col + 1 }, { 'row': this.row + 1, 'col': this.col }, { 'row': this.row + 1, 'col': this.col + 1 }];
+        var same_color_surrounding_bubbles = new Array();
+        for (var i = 0; i < game.fixed_bubbles.length; i++)
+            for (var j = 0; j < surrounding_rcs.length; j++)
+                if (game.fixed_bubbles[i].row == surrounding_rcs[j].row && game.fixed_bubbles[i].col == surrounding_rcs[j].col)
+                    if (Utilities.string_compare(this.color, game.fixed_bubbles[i].color))
+                        same_color_surrounding_bubbles.push(game.fixed_bubbles[i]);
+        if (force && game.stop_keeping_fixed_bubble(this))
+            this.destroy();
+        if (same_color_surrounding_bubbles.length > 0) {
+            if (!force)
+                game.stop_keeping_fixed_bubble(this).destroy();
+            for (var i = 0; i < same_color_surrounding_bubbles.length; i++)
+                same_color_surrounding_bubbles[i].remove(true);
+        }
+    }
+
     update() {
         if (this.movable && this.direction) {
             this.moveVect = this.direction.scale(bubble_speed / Math.sqrt(this.direction.dot(this.direction)));
+            //Touching any bubble?
+            var fixed = false;
             var prediction = this.move_prediction();
             for (var i = 0; i < game.fixed_bubbles.length; i++) {
                 var distance = prediction.distance_to(game.fixed_bubbles[i]);
                 if (distance < min_distance) {
-                    this.fix_location(Bubble.row_and_col(this.coord));
-                    var index;
-                    for (var j = 0; j < game.moving_bubbles.length; j++)
-                        if (game.moving_bubbles[j] === this) {
-                            index = j;
-                            return;
-                        }
-                    game.fixed_bubbles(game.moving_bubbles.splice(index, 1)[0]);
+                    var target_rc = Bubble.row_and_col(this.coord);
+                    while (game.fixed_bubble_at(target_rc) != null)
+                        target_rc.row++;
+                    this.fix_location(target_rc);
+                    game.fixed_bubbles.push(game.stop_keeping_moving_bubble(this));
+                    this.remove(false);
+                    fixed = true;
                     break;
                 }
             }
+            if (!fixed) {
+                //Reaching either side or ceiling?
+                var left = false, right = false, ceil = false;
+                if (prediction.coord.x < bubble_radius)
+                    left = true;
+                else if (prediction.coord.x > canvas.width - bubble_radius)
+                    right = true;
+                if (prediction.coord.y < bubble_radius)
+                    ceil = true;
+                if (ceil) {
+                    this.fix_location(Bubble.row_and_col(prediction.coord));
+                    game.fixed_bubbles.push(game.stop_keeping_moving_bubble(this));
+                } else if (right || left)
+                    this.direction.x = -this.direction.x;
+            }
             CollidableGObject.rm_CollidableGObject_instance_ref(prediction.CollidableGObject_id);
-        }
+        } else if (this.row > game.current_max_row)
+            game.current_max_row = this.row;
         super.update();
     }
 
@@ -264,6 +408,11 @@ class Shooter extends SceneGraph {
         barrel.attach_content(new ShooterBarrel(Math.round(0.382 * w), Math.round(1.1 * w)));
         barrel.assign_name("barrel");
         this.root.attach_child(barrel);
+        var bubble = new SceneGraphNode();
+        var color = bubble_colors[Utilities.getRandomInt(0, bubble_colors.length)];
+        bubble.attach_content(new Bubble(0, 0, bubble_radius, color.r, color.g, color.b, 255));
+        bubble.assign_name("bubble");
+        this.root.attach_child(bubble);
         this.si = game.input_event_subscription_manager.add_subscriber(this);
         this.last_shoot = -1;
     }
@@ -293,12 +442,14 @@ class Shooter extends SceneGraph {
             return;
         var base = this.get_node_by_name("base").content;
         var barrel = this.get_node_by_name("barrel").content;
+        var bubble = this.get_node_by_name("bubble").content;
         var direction = new Vector(barrel.h * Math.sin(barrel.angle / 180 * Math.PI), -barrel.h * Math.cos(barrel.angle / 180 * Math.PI));
         var coord = new Coordinate(this.coord.x + Math.round(base.w / 2), this.coord.y + Math.round(base.h / 2));
         coord.x = Math.round(coord.x + direction.x);
         coord.y = Math.round(coord.y + direction.y);
         var color = bubble_colors[Utilities.getRandomInt(0, bubble_colors.length)];
-        var bubble = new Bubble(coord.x, coord.y, bubble_radius, color.r, color.g, color.b, 255);
+        this.get_node_by_name("bubble").content = new Bubble(0, 0, bubble_radius, color.r, color.g, color.b, 255);
+        bubble.coord = coord;
         bubble.direction = direction;
         game.moving_bubbles.push(bubble);
         this.last_shoot = 0;
@@ -308,7 +459,7 @@ class Shooter extends SceneGraph {
         super.update();
         if (this.last_shoot != -1) {
             this.last_shoot++;
-            if (this.last_shoot == 2 * bubble_radius / bubble_speed)
+            if (this.last_shoot >= 2 * bubble_radius / bubble_speed)
                 this.last_shoot = -1;
         }
     }
@@ -316,6 +467,7 @@ class Shooter extends SceneGraph {
     actual_draw() {
         var base = this.get_node_by_name("base").content;
         var barrel = this.get_node_by_name("barrel").content;
+        var bubble = this.get_node_by_name("bubble").content;
         //Base
         context.save();
         context.translate(this.coord.x, this.coord.y);
@@ -329,11 +481,48 @@ class Shooter extends SceneGraph {
         context.translate(-Math.round(barrel.w / 2), -barrel.h);
         barrel.actual_draw();
         context.restore();
+        //Aim
+        var direction = new Vector(barrel.h * Math.sin(barrel.angle / 180 * Math.PI), -barrel.h * Math.cos(barrel.angle / 180 * Math.PI));
+        var coord = new Coordinate(this.coord.x + Math.round(base.w / 2), this.coord.y + Math.round(base.h / 2));
+        coord.x = Math.round(coord.x + direction.x);
+        coord.y = Math.round(coord.y + direction.y);
+        if (barrel.angle > 0) {
+            var y = coord.y - (canvas.width - bubble_radius - coord.x) / Math.tan(barrel.angle / 180 * Math.PI);
+            var x = canvas.width - bubble_radius - y * Math.tan(barrel.angle / 180 * Math.PI);
+            context.beginPath();
+            context.strokeStyle = "grey";
+            context.lineWidth = 1;
+            context.moveTo(coord.x, coord.y);
+            context.lineTo(canvas.width - bubble_radius, y);
+            context.lineTo(x, 0);
+            context.stroke();
+        } else if (barrel.angle < 0) {
+            var y = coord.y - (coord.x - bubble_radius) / Math.tan(-barrel.angle / 180 * Math.PI);
+            var x = bubble_radius + y * Math.tan(-barrel.angle / 180 * Math.PI);
+            context.beginPath();
+            context.strokeStyle = "grey";
+            context.lineWidth = 1;
+            context.moveTo(coord.x, coord.y);
+            context.lineTo(bubble_radius, y);
+            context.lineTo(x, 0);
+            context.stroke();
+        } else {
+            context.beginPath();
+            context.strokeStyle = "grey";
+            context.lineWidth = 1;
+            context.moveTo(coord.x, coord.y);
+            context.lineTo(coord.x, 0);
+            context.stroke();
+        }
+        //Bubble
+        context.save();
+        context.translate(this.coord.x + base.w / 2, this.coord.y + base.h / 2);
+        bubble.actual_draw();
+        context.restore();
     }
 }
 
 //---------------------------------------------- Run
 
 var game = new BubbleShooter();
-game.init();
-game.start_loop();
+game.start();
